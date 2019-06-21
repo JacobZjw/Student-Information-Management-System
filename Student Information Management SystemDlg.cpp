96a3e83e3fc8e5577c0372523f2362195da5418a
渊 素 <1781053190@qq.com>
2019-06-21 22:08:46 +0800

    添加项目文件。

---
+++ Student Information Management SystemDlg.cpp
@@ -0,0 +1,110 @@
+﻿
+// Student Information Management SystemDlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Student Information Management System.h"
+#include "Student Information Management SystemDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CStudentInformationManagementSystemDlg 对话框
+
+
+
+CStudentInformationManagementSystemDlg::CStudentInformationManagementSystemDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_STUDENTINFORMATIONMANAGEMENTSYSTEM_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CStudentInformationManagementSystemDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CStudentInformationManagementSystemDlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// CStudentInformationManagementSystemDlg 消息处理程序
+
+BOOL CStudentInformationManagementSystemDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	//在窗口左上角设置自己的姓名和学号（课设要求)
+	CString str = "学生成绩管理系统";    
+	//str += theApp.m_us.sName;
+	SetWindowText(str);
+
+	//设置数据显示区
+	CListCtrl* pList = (CListCtrl*)GetDlgItem(IDC_LIST);
+	pList->InsertColumn(5, "姓名", LVCFMT_CENTER, 100);
+	pList->InsertColumn(1, "学号", LVCFMT_CENTER, 110);
+	pList->InsertColumn(2, "高等数学", LVCFMT_CENTER, 100);
+	pList->InsertColumn(3, "课程设计", LVCFMT_CENTER, 100);
+	pList->InsertColumn(4, "总分", LVCFMT_CENTER, 100);
+
+	//设置排序选择按钮
+	CComboBox* pComb = (CComboBox*)GetDlgItem(IDC_SORTCOMBO);   //设置（IDC_SORTCOMBO）CComboBox控件
+	pComb->AddString("学号");
+	pComb->AddString("高数");
+	pComb->AddString("课设");
+	pComb->AddString("总分");
+
+	
+	
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CStudentInformationManagementSystemDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CStudentInformationManagementSystemDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
---
+++ resource.h
@@ -0,0 +1,36 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 StudentInformationManagementSystem.rc 使用
+//
+#define IDD_STUDENTINFORMATIONMANAGEMENTSYSTEM_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDD_ADD_DIALOG                  132
+#define IDC_SORT                        1009
+#define IDC_SORTCOMBO                   1010
+#define IDC_ADDBUTTON                   1011
+#define IDC_DELETEBUTTON                1012
+#define IDC_CHANGEBUTTON                1013
+#define IDC_DATABUTTON                  1014
+#define IDC_IDSEARECH                   1020
+#define IDC_NAMESEARCH                  1021
+#define IDC_SEARCHEDIT                  1022
+#define IDC_SEARCHBUTTON                1023
+#define IDC_LIST                        1024
+#define IDC_EDIT1                       1025
+#define IDC_MAN                         1026
+#define IDC_WOMEN                       1027
+#define IDC_GENDER                      1028
+#define IDC_EDIT2                       1029
+#define IDC_EDIT3                       1030
+#define IDC_EDIT4                       1031
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1032
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
